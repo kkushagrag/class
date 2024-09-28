393bcceb789e943dd485cb7b1989172ab4afcfcf
Kushagra Gangwar <114340521+kkushagrag@users.noreply.github.com>
2024-09-28 16:17:12 +0530

    Update user.controller.js

--- backend/userService/controllers/user.controller.js
+++ backend/userService/controllers/user.controller.js
@@ -1,31 +1,39 @@
-const User = require('../models/user.model').User
+const User = require('../models/user.model').User;
 
+const registerUser = async (req, res) => {
+  try {
+    console.log('User Details:', req.body);
 
-const registerUser = async (req,res) => {
-    try{
-        console.log('user Details: ', req.body)
-        let userData = User({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
-        })
-        await userData.save()
-        res.send('User registered!!!')
-    }catch(err){
-        console.log(err)
-        res.send('Something went wrong')
+    const existingUser = await User.findOne({ email: req.body.email });
+    
+    if (existingUser) {
+      return res.status(400).send('Email is already registered');
     }
-}
 
-const getUser = async(req,res) => {
-    try{
-        User.find({})
-            .then(data => res.send(data))
-            .catch(err => res.send('something went wrong'))
-    }catch(err){
-        console.log(err)
-        res.send('ERR')
-    }
-}
+    let userData = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
 
-module.exports = { registerUser,getUser }
+    await userData.save();
+    
+    res.send('User registered!!!');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Something went wrong');
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    User.find({})
+      .then(data => res.send(data))
+      .catch(err => res.status(500).send('Something went wrong'));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('ERR');
+  }
+};
+
+module.exports = { registerUser, getUser };
